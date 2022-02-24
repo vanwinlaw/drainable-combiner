89f047a2f536c2e967957a97e3ccf99d6ac5c2cb
vanwinlaw <vanwinlaw@gmail.com>
2022-02-24 15:11:05 -0600

    first draft to not show the context option for non-sensical 0.0/1.0 scenarios

--- Contents/mods/Drainable Combiner/media/lua/client/CombineAllFromContextMenu.lua
+++ Contents/mods/Drainable Combiner/media/lua/client/CombineAllFromContextMenu.lua
@@ -13,13 +13,31 @@ end
 
 function isValidToCombine(items, player)
     local character = getSpecificPlayer(player)
+    local totalCombineable = 0
 
     for _, s in pairs(items) do
-        if not instanceof(s, "DrainableComboItem") or character:getInventory():contains(s) ~= true or s:canConsolidate() ~= true then
+        if not instanceof(s, "DrainableComboItem") 
+        or character:getInventory():contains(s) ~= true 
+        or s:canConsolidate() ~= true 
+        then
             return false
         end
+
+        if s:getDelta() < 1.0 and s:getDelta() > 0.0 then
+            totalCombineable = totalCombineable + 1
+        end
     end
-    return true
+    return calculateTotalCombineable(totalCombineable, items) > 1
+end
+
+--[[ calculateTotalCombineable is to account for the first duplicate row and to not account for it as combineable ]]
+function calculateTotalCombineable(total, items)
+    local firstDelta = items[1]:getDelta()
+    if firstDelta == 0.0 or firstDelta == 1.0 then
+        return total
+    end
+
+    return total - 1
 end
 
 Events.OnFillInventoryObjectContextMenu.Add(ISCombineAllMenu.DoContextMenu);
