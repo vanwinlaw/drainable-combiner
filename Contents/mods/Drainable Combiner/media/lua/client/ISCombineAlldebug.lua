6c8f89f61edf4e15653f03ca441a33b44014728d
vanwinlaw <vanwinlaw@gmail.com>
2022-02-28 13:31:59 -0600

    branch debug to allow for a keypress per type

--- Contents/mods/Drainable Combiner/media/lua/client/ISCombineAlldebug.lua
+++ Contents/mods/Drainable Combiner/media/lua/client/ISCombineAlldebug.lua
@@ -33,7 +33,9 @@ if getDebug() then
             player:getInventory():AddItem("Base.Thread", .1)
             player:getInventory():AddItem("Base.Thread", .2)
             player:getInventory():AddItem("Base.Thread", .3)
+        end
 
+        if key == Keyboard.KEY_2 then
             -- Combine down to 4.3 total units of Glue (Expanded/Collapsed)
             player:getInventory():AddItem("Base.Glue", .1)
             player:getInventory():AddItem("Base.Glue", .2)
@@ -46,7 +48,9 @@ if getDebug() then
             player:getInventory():AddItem("Base.Glue", .2)
             player:getInventory():AddItem("Base.Glue", .3)
             player:getInventory():AddItem("Base.Glue", .3)
+        end
 
+        if key == Keyboard.KEY_3 then
             -- Combine down to 3.6 total units of Wood Glue (Expanded/Collapsed)
             player:getInventory():AddItem("Base.Woodglue", .1)
             player:getInventory():AddItem("Base.Woodglue", .2)
@@ -58,7 +62,9 @@ if getDebug() then
             player:getInventory():AddItem("Base.Woodglue", .2)
             player:getInventory():AddItem("Base.Woodglue", .4)
             player:getInventory():AddItem("Base.Woodglue", .3)
+        end
 
+        if key == Keyboard.KEY_4 then
             -- Should not prompt for "Combine All" in collapsed or expanded state
             -- Spawn in some more blow torch to validate that it isn't trying to merge in empty to full/etc or full to empty/etc
             player:getInventory():AddItem("Base.BlowTorch", 0.0)
